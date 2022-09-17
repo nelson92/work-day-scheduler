b37c335d3601d60c8af09041290ada94c5fd16c6
nelson509 <35314794+nelson509@users.noreply.github.com>
2022-09-16 20:59:30 -0700

    color coded by time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,50 @@
 // local storage
 
+
+
+
+
+
+
+
 // color coding text area by time
+// jquery .each for each element
+
+var currentTime = moment().format('hh:mm a')
+console.log(currentTime);
+
+var colorCodes = document.querySelectorAll(".hour");
+var currentHour = moment().hour(); // Number
+// console.log(currentHour);
+
+
+
+$( ".description" ).each(function() {
+  var timeBlock = $(this).attr('id')
+  console.log(timeBlock)
+  if(currentHour > timeBlock){
+    $(this).addClass("past")
+  }else if(currentHour < timeBlock){
+    $(this).addClass("future")
+  }else{
+    $(this).addClass("present")
+  }
+});
+
+// var currentTime = today.format("N");
+
+// if(currentTime > ){
+//   document.getElementByClass("hour").className += "future";
+// } else if (currentTime <  ){
+//   document.getElementByClass("hour").className += "past";
+// } else cdocument.getElementByClass("hour").className += "present"
+
+$( ".saveBtn" ).on( "click", function() {
+  console.log($(this).siblings("textarea").val())
+});
+
+
+
 
 
 var timeDisplayEl = $("#currentDay");
@@ -22,10 +66,10 @@ function displayTime() {
    setInterval(displayTime, 1000);
 
    
-   $(function() {
-     $("#selectable").selectable({
-        placeholder: "ui-widget-content",
-     });
+  //  $(function() {
+  //    $("#selectable").selectable({
+  //       placeholder: "ui-widget-content",
+  //    });
 
-   });
+  //  });
  
